017456c2d0e7144cc7fada46b92f70c9fe2a8ed7
Pedro Candido <pedroh_candido@hotmail.com>
2024-02-20 09:43:35 -0300

    page-javascript

---
+++ src/index.js
@@ -0,0 +1,11 @@
+document.querySelector(".hello1").innerHTML = `Hello World 1`
+document.getElementById("hello2").innerHTML = `Hello World 2`
+document.getElementsByClassName('hello3')[0].innerHTML = `Hello World 3`
+document.querySelectorAll("#hello4")[0].innerHTML = `Hello World 4`
+document.getElementsByClassName('hello5')[0].innerHTML = `Hello World 5`
+document.getElementById("hello6").innerHTML = `Hello World 6`
+document.querySelector(".hello7").innerHTML = `Hello World 7`
+document.querySelectorAll("#hello8")[0].innerHTML = `Hello World 8`
+document.getElementsByClassName('hello9')[0].innerHTML = `Hello World 9`
+document.getElementById("hello10").innerHTML = `Hello World 10`
+
